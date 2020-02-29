ad25bd4124361c1cb72bc650a0e14130614ab3f9
Mikhail Sotnikov <thehofch@gmail.com>
2020-03-01 02:25:44 +0300

    add model service translation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function createTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +19,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (createTranslateLoader),
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/idiot.model.ts
@@ -0,0 +1,44 @@
+const camelCase = require('camelcase');
+const decamelCase = require('decamelize');
+import * as moment from 'moment';
+
+export class Idiot {
+  id: number;
+  name: string;
+  deathDate: string;
+  rStarId: number;
+  pazientDiagnos: string;
+  comment: string;
+  agressive: boolean;
+  momJoke: string;
+  isCheater: boolean;
+  createdAt: string;
+  updatedAt: string;
+
+  fromJson(data): Idiot {
+    Object.keys(data).forEach(k => {
+      this[camelCase(k)] = data[k];
+    });
+    return this;
+  }
+
+  toSnakeCaseObject(): object {
+    const obj: any = {};
+    Object.keys(this).forEach(k => {
+      obj[decamelCase(k)] = this[k];
+    });
+    return obj;
+  }
+
+  get createdAtFormatted(): string {
+    return this.createdAt ? moment(this.createdAt).format('DD.MM.YYYY') : null;
+  }
+
+  get updatedAtFormatted(): string {
+    return this.updatedAt ? moment(this.updatedAt).format('DD.MM.YYYY') : null;
+  }
+
+  get deathDateFormatted(): string {
+    return this.deathDate ? moment(this.deathDate).format('DD.MM.YYYY') : null;
+  }
+}
